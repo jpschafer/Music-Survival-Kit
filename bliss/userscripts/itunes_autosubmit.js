2512fe033ba4e0cde6689577d9a5cc8ebae2e5a2
Jason Schafer <jason59549@gmail.com>
2026-07-03 06:39:54 -0400

    Update script version and improve logging

--- bliss/userscripts/itunes_autosubmit.js
+++ bliss/userscripts/itunes_autosubmit.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ben Dodson Auto Submit
 // @namespace    http://tampermonkey.net/
-// @version      2026-07-03
+// @version      2027-07-03
 // @description  Assists Bliss Image Search Helper
 // @author       You
 // @match        https://bendodson.com/projects/itunes-artwork-finder/*
@@ -12,34 +12,54 @@
     'use strict';
     var RESOLUTION = 1500;
 
-    // Decode the query field if it's URL-encoded before submitting
-    var $query = $('#query');
-    var rawValue = $query.val();
-    if (rawValue) {
-        try {
-            var decoded = decodeURIComponent(rawValue);
-            if (decoded !== rawValue) {
-                $query.val(decoded);
-            }
-        } catch (e) {
-            console.warn('Failed to decode query value:', rawValue, e);
-        }
-    }
+    console.log('[BenDodson] Script loaded. Waiting to process query field...');
 
-    $('input[type="submit"]').trigger('click')
-
-    // Give the page a second to populate results before touching the links
     setTimeout(function() {
-        var list = $('a').filter(function(index) { return $(this).text() === "Standard Resolution"; });
-        var first = list.first();
+        // Decode the query field if it's URL-encoded before submitting
+        var $query = $('#query');
+        var rawValue = $query.val();
+        console.log('[BenDodson] Raw query value:', rawValue);
 
-        if (first.length) {
-            var href = first.attr('href');
-            var newHref = href.replace(/\d+x\d+bb\.jpg$/, RESOLUTION + 'x' + RESOLUTION + '.png');
-            first.attr('href', newHref);
-            window.open(newHref, '_blank');
+        if (rawValue) {
+            try {
+                var decoded = decodeURIComponent(rawValue.replace(/\+/g, ' '));
+                console.log('[BenDodson] Decoded query value:', decoded);
+                if (decoded !== rawValue) {
+                    $query.val(decoded);
+                    console.log('[BenDodson] Query field updated with decoded value.');
+                } else {
+                    console.log('[BenDodson] Decoded value matched raw value, no update needed.');
+                }
+            } catch (e) {
+                console.warn('[BenDodson] Failed to decode query value:', rawValue, e);
+            }
         } else {
-            console.log('No "Standard Resolution" link found.');
+            console.log('[BenDodson] Query field was empty at time of check.');
         }
+
+        console.log('[BenDodson] Triggering submit click.');
+        $('input[type="submit"]').trigger('click')
+
+        // Give the page a second to populate results before touching the links
+        setTimeout(function() {
+            var list = $('a').filter(function(index) { return $(this).text() === "Standard Resolution"; });
+            console.log('[BenDodson] Found', list.length, '"Standard Resolution" link(s).');
+
+            var first = list.first();
+
+            if (first.length) {
+                var href = first.attr('href');
+                console.log('[BenDodson] Original href:', href);
+
+                var newHref = href.replace(/\d+x\d+bb\.jpg$/, RESOLUTION + 'x' + RESOLUTION + '.png');
+                console.log('[BenDodson] Rewritten href:', newHref);
+
+                first.attr('href', newHref);
+                console.log('[BenDodson] Opening rewritten URL in new tab.');
+                window.open(newHref, '_blank');
+            } else {
+                console.log('[BenDodson] No "Standard Resolution" link found.');
+            }
+        }, 1000);
     }, 1000);
 })();
